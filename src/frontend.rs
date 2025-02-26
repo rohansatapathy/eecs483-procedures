66bb7f594b4047d98605de0004c2a4cd4f9d3d20
Rohan Satapathy <61920401+rohansatapathy@users.noreply.github.com>
2025-02-26 16:09:52 -0500

    Implement resolve_prog

--- src/frontend.rs
+++ src/frontend.rs
@@ -6,10 +6,54 @@
 use crate::ast::*;
 use crate::identifiers::*;
 use crate::span::SrcLoc;
+use im::HashMap;
 
 pub struct Resolver {
     pub vars: IdGen<VarName>,
     pub funs: IdGen<FunName>,
+}
+
+#[derive(Debug, Clone)]
+struct EnvFun {
+    name: FunName,
+    arity: usize,
+}
+
+impl EnvFun {
+    fn new(name: FunName, arity: usize) -> Self {
+        Self { name, arity }
+    }
+}
+
+#[derive(Debug, Clone)]
+struct Env {
+    vars: HashMap<String, VarName>,
+    labels: HashMap<String, EnvFun>,
+}
+
+impl Env {
+    fn new() -> Self {
+        Self { vars: HashMap::new(), labels: HashMap::new() }
+    }
+
+    fn insert_var(&mut self, var: &String, var_name: &VarName) {
+        self.vars.insert(var.clone(), var_name.clone());
+    }
+
+    fn get_var_name(&self, var: &String) -> Option<&VarName> {
+        self.vars.get(var)
+    }
+
+    fn insert_label(
+        &mut self, label: &String, fun_name: &FunName, arity: usize,
+    ) {
+        self.labels
+            .insert(label.clone(), EnvFun::new(fun_name.clone(), arity));
+    }
+
+    fn get_env_fun(&self, label: &String) -> Option<&EnvFun> {
+        self.labels.get(label)
+    }
 }
 
 /// CompileErr is an error type that is used to report errors during
@@ -25,7 +69,12 @@ pub enum CompileErr {
     UnboundFunction(String, SrcLoc),
     DuplicateFunction(String, SrcLoc),
     DuplicateParameter(String, SrcLoc),
-    ArityMismatch { name: String, expected: usize, found: usize, loc: SrcLoc },
+    ArityMismatch {
+        name: String,
+        expected: usize,
+        found: usize,
+        loc: SrcLoc,
+    },
 }
 
 impl Resolver {
@@ -33,7 +82,60 @@ impl Resolver {
         Resolver { vars: IdGen::new(), funs: IdGen::new() }
     }
 
-    pub fn resolve_prog(&mut self, prog: SurfProg) -> Result<BoundProg, CompileErr> {
-        todo!("resolve_prog not implemented")
+    pub fn resolve_prog(
+        &mut self, prog: SurfProg,
+    ) -> Result<BoundProg, CompileErr> {
+        let mut env = Env::new();
+
+        // Add main function to environment
+        let name = FunName::Unmangled("entry".to_string());
+        env.insert_label(&prog.name, &name, 1);
+
+        // Add extern functions to environment
+        let externs = prog
+            .externs
+            .iter()
+            .map(|decl| {
+                if let Some(_) = env.get_env_fun(&decl.name) {
+                    return Err(CompileErr::DuplicateFunction(
+                        decl.name.clone(),
+                        decl.loc,
+                    ));
+                }
+
+                let name = FunName::Unmangled(decl.name.clone());
+                let params =
+                    self.resolve_params(&decl.params, &mut env.clone())?;
+                let loc = decl.loc;
+
+                env.insert_label(&decl.name, &name, params.len());
+
+                Ok(BoundExtDecl { name, params, loc })
+            })
+            .collect::<Result<Vec<BoundExtDecl>, _>>()?;
+
+        // Add parameter to environment
+        let param = self.vars.fresh(&prog.param.0);
+        env.insert_var(&prog.param.0, &param);
+
+        Ok(BoundProg {
+            externs,
+            name,
+            param: (param, prog.param.1),
+            body: self.resolve_expr(prog.body, &mut env)?,
+            loc: prog.loc,
+        })
+    }
+
+    fn resolve_params(
+        &mut self, params: &Vec<(String, SrcLoc)>, env: &mut Env,
+    ) -> Result<Vec<(VarName, SrcLoc)>, CompileErr> {
+        todo!()
+    }
+
+    fn resolve_expr(
+        &mut self, expr: SurfExpr, env: &mut Env,
+    ) -> Result<BoundExpr, CompileErr> {
+        todo!()
     }
 }
